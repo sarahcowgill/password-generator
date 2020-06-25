7b2ac658f6e4923cf7a12ece4750b7d7b3fa3738
Sarah Cowgill <sarah.cowgill@yahoo.com>
2020-06-25 15:43:35 -0700

    updating files

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-var character = prompt ("How many characters would you like your password to contain?");
-
+var number = prompt ("How many characters would you like your password to contain?");
+if (number < 8 ) {
+    alert ("Needs to be 8 characters or higher.");
+}
+else if (number > 128 ) {
+    alert ("Needs to be less than 128 characters.")
+}
