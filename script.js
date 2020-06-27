75080812f50dce2fd451af278a9065f80ba7b6c0
Sarah Cowgill <sarah.cowgill@yahoo.com>
2020-06-26 17:44:54 -0700

    updating files

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ function generatePassword() {
     var number = prompt("How many characters would you like your password to contain?");
     if (number < 8) {
         alert("Needs to be 8 characters or higher.");
+        return;
     }
     else if (number > 128) {
         alert("Needs to be less than 128 characters.");
+        return;
     }
     var special = confirm("Click Ok to include special characters.");
 
-    var numeric = confirm("Click Ok to include numbers.")
+    var numeric = confirm("Click Ok to include numbers.");
 
-    var lowercase = confirm("Click Ok to include lowercase letters.")
+    var lowercase = confirm("Click Ok to include lowercase letters.");
 
-    var uppercase = confirm("Click Ok to include uppercase letters.")
+    var uppercase = confirm("Click Ok to include uppercase letters.");
+
+    document.getElementById(“password”).textContent = “aRandomPassword”;
+
+
 }
 
 
