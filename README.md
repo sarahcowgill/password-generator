5dc82e65e605fcfb6facbef3d55703675d19cc12
Sarah Cowgill <sarah.cowgill@yahoo.com>
2020-06-28 20:38:47 -0700

    updating files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # password-generator
-Random password generator using Javascript
+In this project I created a random password generator using Javascript. The user gets to select how many characters they would like and what types of characters that will be randomly selected in their password. This was my first time using Javascript and I found the project very challenging. 
+
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
 function generatePassword() {
+    //Collect the password settings from the user. Here asking for how many characters. 
     var number = prompt("How many characters would you like your password to contain?");
     if (number < 8) {
         alert("Needs to be 8 characters or higher.");
@@ -9,6 +9,7 @@ function generatePassword() {
         alert("Needs to be less than 128 characters.");
         return;
     }
+    //Giving the user the options to include different types of characters. 
     var special = confirm("Click Ok to include special characters.");
 
     var numeric = confirm("Click Ok to include numbers.");
@@ -17,6 +18,7 @@ function generatePassword() {
 
     var uppercase = confirm("Click Ok to include uppercase letters.");
 
+   //Creating a variable for the console for debugging purposes. 
     var passwordSettings = {
         "number": number,
         "special": special,
@@ -26,12 +28,14 @@ function generatePassword() {
     };
     console.log(passwordSettings);
 
-
+    //Alerting the user they must choose at least one character class, if they don't select at least one character class. 
     if (!special && !numeric && !lowercase && !uppercase) {
         alert("Please select at least one password character class.");
         return;
     }
-    var passwordChars = ""
+
+    //Listing all the possible characters that could be generated. 
+    var passwordChars = "";
     var specialChars = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
     if (special) {
         passwordChars = passwordChars + specialChars;
@@ -48,18 +52,18 @@ function generatePassword() {
     if (uppercase) {
         passwordChars = passwordChars + upperCaseChars;
     }
-
+    //Declare a variable to hold the random password string,initially empty. 
     var aRandomPassword = "";
 
     var numPasswordChars = passwordChars.length;
-
+    //Loop for the number of requested password characters, adding a randomly chosen character on each iteration. 
     for (i = 0; i < number; i++) {
         var randomPosition = Math.floor(Math.random() * numPasswordChars);
         var randomCharacter = passwordChars.charAt(randomPosition);
         aRandomPassword = aRandomPassword + randomCharacter;
     }
 
-
+    //Display the generated password on the page. 
     document.getElementById("password").textContent = aRandomPassword;
 
 
